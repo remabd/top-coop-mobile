d50100bd6ab3ddb0a66a7189f54124b41d8fcb05
remabd <remiabdallah3@gmail.com>
2026-06-15 12:21:04 +0200

    feat-Api: Met à jour l'appel API pour valider un panier

--- src/api/panier.api.ts
+++ src/api/panier.api.ts
@@ -1,25 +1,14 @@
-import axios from 'axios';
+import { api, request } from './client';
+import { loadToken } from '../store/securetoken';
 import { Panier } from '../models/panier.type';
 
-export class PanierApi {
-  private token: string;
-  private baseUrl: string;
-  constructor() {
-    this.token = '';
-    this.baseUrl = process.env.EXPO_PUBLIC_API_URL + '/panier';
-  }
-
-  public async validePanier(panier: Panier) {
-    try {
-      const response = await axios.post<Panier>('/', panier, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.token}`,
-        },
-      });
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+export async function demandeValidationPanier(panier: Panier) {
+  const token = await loadToken();
+  return request<Panier>(
+    api.post('/panier', panier, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  );
 }
